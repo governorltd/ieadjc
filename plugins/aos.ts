4c81a1cf85d5a608992a4c2ca60b094753dc8f4d
Claudemir Casa <claudemir.casa@ufpr.br>
2023-12-20 18:42:53 -0300

    update for deploy

--- plugins/aos.ts
+++
@@ -1,8 +0,0 @@
-import AOS from "aos"
-import "aos/dist/aos.css"
-
-export default defineNuxtPlugin((nuxtApp) => {
-    if (typeof window !== "undefined") {
-        nuxtApp.AOS = AOS.init({ disable: "phone", once: true, offset: 10 }) // eslint-disable-line new-cap
-    }
-})
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  resolve: {
-    alias: {
-      'vue-easy-lightbox': 'vue-easy-lightbox/dist/external-css/vue-easy-lightbox.esm.min.js'
-    }
-  }
-})
